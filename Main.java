40d05b420ae2eec5de283ee5e10a56a3b061ca2b
ShhhD20 <142339741+ShhhD20@users.noreply.github.com>
2024-01-25 11:40:23 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,16 +1,43 @@
-package proceso;
+package Figuras;
+import java.util.Scanner;
 
-public class Main {
+public class Instrucciones {
 	public static void main(String[] args) {
-		
 		Cuadrado calculoCuad = new Cuadrado();
 		Circulo calculoCirc = new Circulo();
 		
-		System.out.println("Diagonal del cuadrado: " + calculoCuad.diagonal(1822.38));
-		System.out.println("Perimetro del cuadrado: " + calculoCuad.perimetro(1822.38));
-		System.out.println("Area del cuadrado: " + calculoCuad.area(1822.38));
-		System.out.println(" ");
-		System.out.println("Perimetro del circulo: " + calculoCirc.circunferencia(1822.38));
-		System.out.println("Area del circulo: " + calculoCirc.area(1822.38));
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Que figura quiere hacer?\n\t[1] Cuadrado\n\t[2] Circulo");
+		String figura = scan.nextLine();
+		int intEscaneado = Integer.parseInt(figura);
+		System.out.println("Que medida el lado/radio?");
+		String lado = scan.nextLine();
+		double douLado = Double.parseDouble(lado);
+		
+		switch(intEscaneado) {
+			case 1:
+				System.out.println("Que desea calcular?\n\t[1] Diagonal\n\t[2] Perimetro\n\t[3] Area");
+				String operacion = scan.nextLine();
+				int intOperacion = Integer.parseInt(operacion);
+				switch(intOperacion) {
+					case 1:
+						System.out.println("La diagonal mide: " + calculoCuad.diagonal(douLado)); break;
+					case 2:
+						System.out.println("El perimetro mide: " + calculoCuad.perimetro(douLado)); break;
+					case 3:
+						System.out.println("El area mide: " + calculoCuad.area(douLado)); break;
+				} break;
+			case 2:
+				System.out.println("Que desea calcular?\n\t[1] Circunferencia\n\t[2] Area");
+				String oper = scan.nextLine();
+				int intOper = Integer.parseInt(oper);
+				switch(intOper) {
+				case 1:
+					System.out.println("La circunferencia mide: " + calculoCirc.circunferencia(douLado)); break;
+				case 2:
+					System.out.println("El area mide: " + calculoCirc.area(douLado)); break;
+			} break;
+		}
+		scan.close();
 	}
 }
